038e6736a86ae81a267250fc6349f40b32f45d3a
patkamon <patkamon.a@ku.th>
2022-03-06 13:24:42 +0700

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+RICE.
+
+`npm install`  
+`npm start`
+
+[doc](https://docs.google.com/document/d/1Zgktz3rl3L3etOMBdXSVNMAD0m041fJvvSHRQ0sSNPg/edit?usp=sharing)
---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+const { WALLET_PRIVATE_KEY,ALCHEMY } = process.env;
+
+module.exports = {
+  defaultNetwork: "mumbai",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: [`${ALCHEMY}`],
+      accounts: [`0x${WALLET_PRIVATE_KEY}`]
+    }
+  },
+  solidity: "0.8.4",
+};
