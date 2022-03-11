f9b971fa8fb962fe1e34f31380477392967d7786
Thanabardi <Thanabardi@hotmail.com>
2022-03-11 15:20:49 +0700

    change API URL

--- src/pages/Vote.jsx
+++ src/pages/Vote.jsx
@@ -10,40 +10,30 @@ import '../assets/Vote.css';
 const Vote = () => {
 	const bearerToken = process.env.REACT_APP_TWITTER_API_KEY
 
-	let [candidateIDs, setCandidateID] = useState([]) //list of candidate id
+	let [candidateIDs, setCandidateID] = useState(['9366932', '2864086918', '1283657064410017793']) //list of candidate id
 	let [candidateList, setCandidateList] = useState([]) // list candidate details(id, name, screen name, followers, profile image)
 	let [select, setSelect] = useState({}) // a candidate that user selected
 
-  // useEffect(() => {
-	// 	// create candidate details list from candidate id
-  //   for (let i = 0; i < candidateIDs.length; i++) {
-	// 		getAccountProfile(candidateIDs[i])
-	// 	}
-  // });
+  useEffect(() => {
+		// create candidate details list from candidate id
+		getAccountProfile(candidateIDs)
+  }, []);
 
-	// async function getAccountProfile(accountID) {
- 	// 	await axios.get(`/1.1/users/show.json?user_id=${accountID}`, {
-	// 		"headers": {
-	// 			'Authorization': `Bearer ${bearerToken}`
-	// 		}
-	// 	})
-	// 	.then(response => {
-	// 		// console.log(response.data)
-	// 		const temp = {
-	// 			id_str: response.data.id_str, 
-	// 			name: response.data.name, 
-	// 			screen_name: response.data.screen_name,
-	// 			followers: response.data.followers_count,
-	// 			profile_image: response.data.profile_image_url_https.replace("_normal", "") // to make original size profile image
-	// 		}
-	// 		let newList = candidateList.concat(temp)
-	// 		setCandidateList(newList)
-	// 	})
-	// 	.catch(error => {
-	// 		window.alert(error)
-	// 		// console.log(error)
-	// 	})
-  // }
+	async function getAccountProfile() {
+ 		await axios.get(`/1.1/users/lookup.json?user_id=${candidateIDs.toString()}`, {
+			"headers": {
+				'Authorization': `Bearer ${bearerToken}`
+			}
+		})
+		.then(response => {
+			// console.log(response.data)
+			setCandidateList(response.data)
+		})
+		.catch(error => {
+			window.alert(error)
+			// console.log(error)
+		})
+  }
 
 	function addCandidate(account) {
 		// console.log(account)
@@ -54,15 +44,8 @@ const Vote = () => {
 			let temp = candidateIDs.concat(account.id_str)
 			setCandidateID(temp)
 			// add Candidate's account profile into CandidateList
-			temp = {
-				id_str: account.id_str, 
-				name: account.name, 
-				screen_name: account.screen_name,
-				followers: account.followers_count,
-				profile_image: account.profile_image_url_https.replace("_normal", "")
-			}
-			const newList = candidateList.concat(temp)
-			setCandidateList(newList)
+			temp = candidateList.concat(account)
+			setCandidateList(temp)
 		}
 	}
 
@@ -83,11 +66,12 @@ const Vote = () => {
         </thead>
 				<tbody>
 					{candidateList.map((candidate, index) => {
+						const profile_image = candidate.profile_image_url_https.replace("_normal", "")
             return (
               <tr key={index} className={candidate.id_str === select.id_str ? "select" : "table"}>
 								<td className='vote-td'><button className='vote-select-button' value={candidate} onClick={() => setSelect(candidate)}>select</button></td>
                 <td className='vote-td'>{candidate.name}
-								<ShowUser accountProfile={[candidate.id_str, candidate.name, candidate.screen_name, candidate.followers, candidate.profile_image]} /></td>
+								<ShowUser accountProfile={[candidate.id_str, candidate.name, candidate.screen_name, candidate.followers_count, profile_image]} /></td>
               </tr>
             );
           })}
