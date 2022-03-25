b7941c13a267f1b957746ffd28a6154e6e2c34d2
Thanabardi <Thanabardi@hotmail.com>
2022-03-25 23:44:15 +0700

    improve template

--- src/App.js
+++ src/App.js
@@ -7,20 +7,12 @@ import StakePage from './pages/StakePage'
 import VotePage from './pages/VotePage'
 import AdminPage from './pages/AdminPage'
 
-function App() {
-  const path_rice = localStorage.state;
-  const navigate = useNavigate();
+import NavBar from './components/NavBar'
 
+function App() {
   return (
     <div className="App">
-      <center>
-      <div className='topnav'>
-        <div onClick={()=>{localStorage.state="swap";  navigate('/swap')}} style={{backgroundColor:path_rice==="swap"?'#55d6a7':'white'}}>swap</div>
-        <div onClick={()=>{localStorage.state="stake"; navigate('/stake')}} style={{backgroundColor:path_rice==="stake"?'#55d6a7':'white'}}>stake</div>
-        <div onClick={()=>{localStorage.state="vote"; navigate('/vote')}} style={{backgroundColor:path_rice==="vote"?'#55d6a7':'white'}}>vote</div>
-        <div onClick={()=>{localStorage.state="admin"; navigate('/admin')}} style={{backgroundColor:path_rice==="admin"?'#55d6a7':'white'}}>admin</div>
-      </div>
-      </center>
+      <NavBar />
       <Routes>
         <Route path="/swap" element={<SwapPage />} />
         <Route path="/stake" element={<StakePage />} />
--- src/pages/VotePage.jsx
+++ src/pages/VotePage.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 import UserSearch from '../components/UserSearch'
 import ShowUser from '../components/ShowUser'
+import VotePopup from '../components/VotePopup'
 
 import '../assets/VotePage.css';
 
@@ -11,6 +12,7 @@ const Vote = () => {
 	const bearerToken = process.env.REACT_APP_TWITTER_API_KEY
 
 	let [candidateIDs, setCandidateID] = useState(['9366932', '2864086918', '1283657064410017793']) //list of candidate id
+	let [newCandidateList, setNewCandidateList] = useState([]) //list of new candidate id
 	let [candidateList, setCandidateList] = useState([]) // list candidate details(id, name, screen name, followers, profile image)
 	let [select, setSelect] = useState({}) // a candidate that user selected
 
@@ -43,46 +45,64 @@ const Vote = () => {
 			// add accountID into database
 			let temp = candidateIDs.concat(account.id_str)
 			setCandidateID(temp)
-			// add Candidate's account profile into CandidateList
-			temp = candidateList.concat(account)
-			setCandidateList(temp)
-		}
-	}
-
-	function vote() {
-		if (select !== {}) {
-			window.alert(`Vote ${select.name} (@${select.screen_name})?`)
-			console.log(`voted user ID ${select.id_str}`)
-		} else {
-			window.alert(`Please select your candidate.`)
+			// add Candidate's account profile into NewCandidateList
+			temp = newCandidateList.concat(account)
+			setNewCandidateList(temp)
 		}
 	}
 
   return (
-    <div>
-      <table className='vote-table'>
-        <thead >
-					<tr><td colSpan="2" style={{fontSize: "25px", fontWeight: "bold", textAlign: "center"}}>Vote</td></tr>
-        </thead>
-				<tbody>
-					{candidateList.map((candidate, index) => {
-						const profile_image = candidate.profile_image_url_https.replace("_normal", "")
-            return (
-              <tr key={index} className={candidate.id_str === select.id_str ? "select" : "table"}>
-								<td className='vote-td'><button className='vote-select-button' value={candidate} onClick={() => setSelect(candidate)}>select</button></td>
-                <td className='vote-td'>{candidate.name}
-								<ShowUser accountProfile={[candidate.id_str, candidate.name, candidate.screen_name, candidate.followers_count, profile_image]} /></td>
-              </tr>
-            );
-          })}
-					<tr style={{lineHeight: "50px"}}>
-						<td className='vote-td' style={{borderBottom: "0px"}}>Other:</td>
-						<td className='vote-td' style={{borderBottom: "0px"}}><UserSearch sendData={addCandidate} /></td>
-					</tr>
-				</tbody>
-      </table>
-			<button className='vote-button' onClick={vote}>Vote</button>
-    </div>
+		<div className='vote'>
+			<div style={{padding: "20px", fontSize: "30px"}}>Vote</div>
+			<div className='vote-div'>
+				<table className='vote-table'>
+					{/* <thead >
+						<tr><td colSpan="2" style={{fontSize: "25px", fontWeight: "bold", textAlign: "center"}}>Vote</td></tr>
+					</thead> */}
+					<tbody>
+						{candidateList.map((candidate, index) => {
+							const profile_image = candidate.profile_image_url_https.replace("_normal", "")
+							return (
+								<tr key={index} className={candidate.id_str === select.id_str ? "select" : "table"}>
+									<td className='vote-td'><img src={profile_image} alt="Account Profile" style={{borderRadius: "100%", width: "50px"}} 
+										onClick={() => setSelect(candidate)} className='vote-select-button'/></td>
+									<td className='vote-td' ><button className='vote-select-button' value={candidate} 
+										onClick={() => setSelect(candidate)}>{candidate.name}</button>
+									<ShowUser accountProfile={[candidate.id_str, candidate.name, candidate.screen_name, candidate.followers_count, profile_image]} /></td>
+								</tr>
+							);
+						})}
+						{/* <tr style={{lineHeight: "50px"}}>
+							<td className='vote-td' style={{borderBottom: "0px"}}>Other:</td>
+							<td className='vote-td' style={{borderBottom: "0px"}}><UserSearch sendData={addCandidate} /></td>
+						</tr> */}
+					</tbody>
+				</table>
+			</div>
+			<div style={{padding: "20px", fontSize: "20px"}}>Add Your New Candidate</div>
+			<div className='vote-div'>
+				<table className='vote-table'>
+					<tbody>
+						{newCandidateList.map((candidate, index) => {
+							const profile_image = candidate.profile_image_url_https.replace("_normal", "")
+							return (
+								<tr key={index} className={candidate.id_str === select.id_str ? "select" : "table"}>
+									<td className='vote-td'><img src={profile_image} alt="Account Profile" style={{borderRadius: "100%", width: "50px"}} 
+										onClick={() => setSelect(candidate)} className='vote-select-button'/></td>
+									<td className='vote-td' ><button className='vote-select-button' value={candidate} 
+										onClick={() => setSelect(candidate)}>{candidate.name}</button>
+									<ShowUser accountProfile={[candidate.id_str, candidate.name, candidate.screen_name, candidate.followers_count, profile_image]} /></td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+				<div style={{paddingTop: newCandidateList.length > 0 ? "20px": "0"}}>
+					<UserSearch sendData={addCandidate} />
+				</div>
+			</div>
+			<VotePopup voteAccount={select}/>
+		</div>
   );
 }
 export default Vote;
