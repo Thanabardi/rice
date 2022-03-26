a953a54351e78e5b62025d2c49b151278c31575d
Kittison <kittison.j@ku.th>
2022-03-26 20:54:01 +0700

    SwapPage can swap and transfer coin

--- src/pages/SwapPage.jsx
+++ src/pages/SwapPage.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers'
 import WMATIC from '../artifacts/contracts/WMatic.sol/WMATIC.json'
 import Swap from '../artifacts/contracts/Swap.sol/Swap.json'
 import matic from '../assets/images/matic.png';
 import rice from '../assets/images/rice.jpg';
 import '../assets/SwapPage.css';
-// import Box from '@mui/material/Box';
-// import InputLabel from '@mui/material/InputLabel';
-// import MenuItem from '@mui/material/MenuItem';
-// import FormControl from '@mui/material/FormControl';
-// import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const SwapPage = () => {
   const swapAddress = '0x9DDe8618a3713aE483E4976Ae8427A040d9f931B'
 
   // TODO: change address Rice and Matic
   const coinOption  = [
-    {value: "Matic address", label: "Matic", img:matic},
-    {value: "Rice address", label: "Rice", img:rice}
+    {value: "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889", label: "Matic", img:matic},
+    {value: "0x87C2EBffe6C50eE034b4D05D2d3c2EC7b325e346", label: "Rice", img:rice}
   ]
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -51,26 +51,69 @@ const SwapPage = () => {
       }, 20000);
     }
   }
+  function add(x, y) {
+    var c = 0, r = [];
+    var x = x.split('').map(Number);
+    var y = y.split('').map(Number);
+    while(x.length || y.length) {
+            var s = (x.pop() || 0) + (y.pop() || 0) + c;
+            r.unshift(s < 10 ? s : s - 10); 
+            c = s < 10 ? 0 : 1;
+    }
+    if(c) r.unshift(c);
+    return r.join('');
+  }
 
-  async function  onGetOdds(e){
-    e.preventDefault()
+  function h2d(s) {
+  var dec = '0';
+  s.split('').forEach(function(chr) {
+      var n = parseInt(chr, 16);
+      for(var t = 8; t; t >>= 1) {
+          dec = add(dec, dec);
+          if(n & t) dec = add(dec, '1');
+      }
+  });
+  return dec;
+  }
+
+  async function  onGetOdds(){
     if (typeof window.ethereum !== 'undefined') {
       const provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.maticvigil.com')
       console.log({ provider })
       const contract = new ethers.Contract(swapAddress, Swap.abi, provider)
-      try {
-        const data = await contract.getTokenOdds(coinState1, coinState2,  e.target[2].value*100000 + "0000000000000",)
-
-        console.log('Total: ', data)
-      } catch (err) {
-        console.log("Error: ", err)
-      }
+          try {
+            const data = await contract.getTokenOdds(coinState1, coinState2,  amountState*100000 + "0000000000000",)
+            setHex(data._hex);
+            const hexToDec = h2d(data._hex);
+            if (data._hex==="0x00"){
+              document.getElementById("Alert").innerHTML = "Error Insufficient Amount"
+            }
+            else {
+              document.getElementById("Alert").innerHTML = ""
+            }
+            setCalculateState(hexToDec/Math.pow(10, 18));
+            console.log('Total: ', data)
+          } catch (err) {
+            console.log("Error: ", err)
+          }
     }  
   }
 
-  const [coinState1, setState1] = useState("");
-  const [coinState2, setState2] = useState("");
-  const [amountState, setAmountState] = useState("");
+  function isSufficient(){
+    // const ele = document.getElementById("value").value
+    console.log("insuffiecient ", amountHex)
+    if (amountHex==='0x00'){
+      document.getElementById("Summit").disabled = true;
+    } else {
+      document.getElementById("Summit").disabled = false;
+    }
+  }
+
+  const [coinState1, setState1] = useState('0');
+  const [coinState2, setState2] = useState('0');
+  const [amountState, setAmountState] = useState('0');
+  const [amountHex, setHex] = useState('0');
+  const [amountCalculation, setCalculateState] = useState('0');
 
   const handleChange1 = (event) => {
     setState1(event.target.value);
@@ -83,64 +126,78 @@ const SwapPage = () => {
   };
 
   const handleChange3 = (event) => {
-    // TODO: set CalculateCoin
     setAmountState(event.target.value);
-    console.log(event.target.value)
-  };
+    console.log(event.target.value);
+    onGetOdds();
+  }
 
   return (
     <div className='swap'>
       Swap
-      <center>
-        {/* <Box sx={{ maxWidth: 120 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">COIN</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={coinState1}
-              label="Coin"
-              onChange={handleChange1}
-            >
-              <MenuItem value={coinOption[0].value}>Matic <img src={coinOption[0].img}/></MenuItem>
-              <MenuItem value={coinOption[1].value}>Rice <img src={coinOption[1].img}/></MenuItem>
-            </Select>
-          </FormControl>
-        </Box> */}
-      </center>
-      <center>
-        {/* <Box sx={{ maxWidth: 120 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">COIN</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={coinState2}
-              label="Coin"
-              onChange={handleChange2}
-            >
-              <MenuItem value={coinOption[0].value}>Matic <img src={coinOption[0].img}/></MenuItem>
-              <MenuItem value={coinOption[1].value}>Rice <img src={coinOption[1].img}/></MenuItem>
-            </Select>
-          </FormControl>
-        </Box> */}
-      </center>
-      <form onSubmit={onSwap}>
-        <input type="hidden" value={coinState1} disabled={coinState1===""}></input>
-        <input type="hidden" value={coinState2} disabled={coinState1===""}></input>
-        <input placeholder='amount token' type='number' step=".0001" onChange={handleChange3}></input><br/>
-
-        <button>submit</button>
-      </form>
-      GetOdds
-      <form onSubmit={onGetOdds}>
-        <input type="hidden" value={coinState1} disabled={coinState1===""}></input>
-        <input type="hidden" value={coinState2} disabled={coinState1===""}></input>
-        {/* TODO: set value and render coin */}
-        <input placeholder='amount token' type='number' step=".0001" disabled="true"></input><br/>
-        <button>submit</button>
-      </form>
-    </div>
+        <div style={{ margin_top: '5%'}}>
+          <form onSubmit={onSwap}> 
+              <input className='swapInput' type="hidden" value={coinState1} disabled={coinState1===""}></input>
+              <input className='swapInput' type="hidden" value={coinState2} disabled={coinState1===""}></input>
+              <div className='input-item'>
+                <input id='value' className='swapInput' placeholder='amount token' type='number' step=".0001" onInput={handleChange3}></input>
+                <div className='swapInner'>
+                    <div className='swapSelect'>
+                      <div className='swapInterface'>
+                        <Box sx={{ width:"125px" }}>
+                          <FormControl fullWidth required>
+                            <InputLabel id="demo-simple-select-label">COIN</InputLabel>
+                              <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={coinState1}
+                                label="Coin"
+                                onChange={handleChange1}
+                                sx={{
+                                  bgcolor:"white"
+                                }}
+                              >
+                                  <MenuItem value={coinOption[0].value}>Matic <img className="imageChange" src={coinOption[0].img}/></MenuItem>
+                                  <MenuItem value={coinOption[1].value}>Rice <img className="imageChange" src={coinOption[1].img}/></MenuItem>
+                              </Select>
+                          </FormControl>
+                      </Box>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <br/>
+              <div className='input-item'>
+            <input className='swapInput' value={amountCalculation} placeholder='amount token' type='number' step=".0001" disabled></input><br/>
+            <div className='swapInner'>
+            <div className='swapSelect'>
+              <div className='swapInterface'>
+                <Box sx={{ width:"125px" }}>
+                  <FormControl fullWidth required>
+                    <InputLabel id="demo-simple-select-label">COIN</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={coinState2}
+                        label="Coin"
+                        onChange={handleChange2}
+                        sx={{
+                          bgcolor:"white"
+                        }}
+                      >
+                          <MenuItem value={coinOption[0].value}>Matic <img className='imageChange' src={coinOption[0].img}/></MenuItem>
+                          <MenuItem value={coinOption[1].value}>Rice <img className='imageChange' src={coinOption[1].img}/></MenuItem>
+                      </Select>
+                  </FormControl>
+                </Box>
+              </div>
+            </div>
+            </div>
+            </div>
+              <button id="Summit" className='swapSummit' disabled={amountHex==="0x00"?true :false}>Enter amount</button>
+          </form>
+          <p className='Alert' id="Alert"></p>
+        </div>
+      </div>
   );
 }
 export default SwapPage;
