5d184bd5fc8d4e353e7424e607dfd8837b400637
patkamon <patkamon.a@ku.th>
2022-03-27 01:04:15 +0700

    take2

--- src/pages/VotePage.jsx
+++ src/pages/VotePage.jsx
@@ -19,7 +19,7 @@ const factoryAddress = "0x434Cbdedc7A8069C5F2426C617C3858Bc88014d3"
 const Vote = () => {
 	const bearerToken = process.env.REACT_APP_TWITTER_API_KEY
 
-	let [candidateIDs, setCandidateID] = useState([]) //list of candidate id
+	let [candidateIDs, setCandidateID] = useState(["44196397"]) //list of candidate id
 	let [newCandidateList, setNewCandidateList] = useState([]) //list of new candidate id
 	let [candidateList, setCandidateList] = useState([]) // list candidate details(id, name, screen name, followers, profile image)
 	let [select, setSelect] = useState({}) // a candidate that user selected
@@ -178,10 +178,9 @@ const Vote = () => {
 
   return (
 		<div className='vote'>
-			{!award && "RICE: "+ voteAmount}<br/>
-			{!award && "Session is on going"}<br/>
-			{award && "Session is ended!!"}<br/>
-			{award && "Winner is: "+ winner}<br/>
+			{!award && "RICE: "+ voteAmount}{award && "Session is ended!!"}<br/>
+			{!award && "Session is on going"}{award && "Winner is: "+ winner}<br/>
+			
 			{award && "Award is: "+ award}<br/>
 			<div style={{padding: "20px", fontSize: "30px"}}>Vote</div>
 			<div className='vote-div'>
@@ -190,7 +189,7 @@ const Vote = () => {
 						<tr><td colSpan="2" style={{fontSize: "25px", fontWeight: "bold", textAlign: "center"}}>Vote</td></tr>
 					</thead> */}
 					<tbody>
-						{candidateList &&  candidateList.map((candidate, index) => {
+						{candidateList.length!==0 &&  candidateList.map((candidate, index) => {
 							const profile_image = candidate.profile_image_url_https.replace("_normal", "")
 							return (
 								<tr key={index} className={candidate.id_str === select.id_str ? "select" : "table"}>
@@ -213,7 +212,8 @@ const Vote = () => {
 			<div className='vote-div'>
 				<table className='vote-table'>
 					<tbody>
-						{newCandidateList && newCandidateList.map((candidate, index) => {
+						{
+							newCandidateList.map((candidate, index) => {
 							const profile_image = candidate.profile_image_url_https.replace("_normal", "")
 							return (
 								<tr key={index} className={candidate.id_str === select.id_str ? "select" : "table"}>
