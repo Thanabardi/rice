fb6a74ef7d34973f8c22d11c2d980b3f5c5585b0
patkamon <patkamon.a@ku.th>
2022-04-02 21:41:32 +0700

    network button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 
 import '../assets/NavBar.css';
 
 const NavBar = ({}) => {
   const navigate = useNavigate();
+  let [networkStatus, setNetworkStatus] = useState("Wrong Network")
   const path = window.location.pathname;
 
   useEffect(() => {
   // ask for permission to go to mumbai network
+  if (typeof window.ethereum !== 'undefined') {
   window.ethereum.request({
     method: "wallet_addEthereumChain",
     params: [{
@@ -22,12 +24,27 @@ const NavBar = ({}) => {
       },
       blockExplorerUrls: ["https://mumbai.polygonscan.com/"]
     }]
-  });
-},[]);
+  })
+  setNetworkStatus(window.ethereum.networkVersion == '80001' ? "Maticmum" : "Wrong Network")
+
+  const interval = setInterval(() => {
+    handleCheck()
+  }, 3000);
+
+}},[]);
+
+function handleCheck(){
+  setNetworkStatus(window.ethereum.networkVersion == '80001' ? "Maticmum" : "Wrong Network")
+  console.log('switch')
+
+}
 
 
   return (
     <div>
+
+    <button className={networkStatus === "Maticmum" ? "maticmum":"others"} onClick={handleCheck} >{networkStatus === "Maticmum" ? "Matic Mumbai" : "Wrong Network"}</button>
+
       <div className='nav-bar'>
         <button onClick={()=>{localStorage.state="swap";  navigate('/swap')}} 
           className={path === "/swap" ? 'nav-select' : 'nav-not-select'}>Swap</button>
